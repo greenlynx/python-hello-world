73e29323af56dfe5fbedb65f60f1cf78b7ea1441
Dan Haughey <dan@greenlynx.co.uk>
2022-06-11 12:21:41 +0100

    WIP

--- src/handlers/http/hello.py
+++ src/handlers/http/hello.py
@@ -1,8 +1,12 @@
 import json
-import string
 
 from api_gw import APIGWPayloadV2RequestContextDict, APIGWPayloadV2RequestDict
 
 
-def handler(event: APIGWPayloadV2RequestDict, context: APIGWPayloadV2RequestContextDict):
-    return {"statusCode": 200, "body": json.dumps({"message": "Hello world!", "path": event["rawPath"]})}
+def handler(
+    event: APIGWPayloadV2RequestDict, context: APIGWPayloadV2RequestContextDict
+):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Hello world!", "padth": event["rawPath"]}),
+    }
