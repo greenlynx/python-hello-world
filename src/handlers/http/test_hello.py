15ff312f3aba0fe7e3d0337e0749126fda7e6e17
Dan Haughey <dan@greenlynx.co.uk>
2022-06-12 13:37:54 +0100

    WIP

--- src/handlers/http/test_hello.py
+++ src/handlers/http/test_hello.py
@@ -6,4 +6,4 @@ import hello
 def test_it_says_hello():
     result = hello.handler({}, {})
     assert result["statusCode"] == 200
-    assert result["body"] == json.dumps({"message": "Hello world!"})
+    assert result["body"] == json.dumps({"message": "Hello world!!"})
