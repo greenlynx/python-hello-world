c52d2993b685664743c27be63993ddf7193d0c43
Dan Haughey <dan@greenlynx.co.uk>
2022-06-13 21:26:32 +0100

    WIP

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from aws_cdk import App, Stack
-from aws_cdk import aws_apigateway as _apigw
-from aws_cdk import aws_lambda as _lambda
+from aws_cdk import App, Stack, aws_apigateway, aws_lambda
 from constructs import Construct
 
 # pylint: disable=too-few-public-methods
@@ -10,26 +8,30 @@ class HelloLambdaStack(Stack):
     def __init__(self, scope: Construct, id_: str, **kwargs) -> None:
         super().__init__(scope, id_, **kwargs)
 
-        base_lambda = _lambda.Function(
+        baseaws_lambda = aws_lambda.Function(
             self,
             "HelloLambda",
             handler="src/handlers/http/hello.handler",
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            code=_lambda.Code.from_asset("src"),
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            code=aws_lambda.Code.from_asset("src"),
         )
 
-        base_api = _apigw.RestApi(self, "ApiGateway", rest_api_name="ApiGateway")
+        base_api = aws_apigateway.RestApi(
+            self, "ApiGateway", rest_api_name="ApiGateway"
+        )
 
         example_entity = base_api.root.add_resource("hello")
-        example_entity_lambda_integration = _apigw.LambdaIntegration(
-            base_lambda,
+        example_entityaws_lambda_integration = aws_apigateway.LambdaIntegration(
+            baseaws_lambda,
             proxy=False,
-            integration_responses=[_apigw.IntegrationResponse(status_code="200")],
+            integration_responses=[
+                aws_apigateway.IntegrationResponse(status_code="200")
+            ],
         )
         example_entity.add_method(
             "GET",
-            example_entity_lambda_integration,
-            method_responses=[_apigw.MethodResponse(status_code="200")],
+            example_entityaws_lambda_integration,
+            method_responses=[aws_apigateway.MethodResponse(status_code="200")],
         )
 
 
