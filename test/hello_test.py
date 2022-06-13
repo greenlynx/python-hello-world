c4ab6b62a3f84f86d9c7b48ae4f81725bef39baf
Dan Haughey <dan@greenlynx.co.uk>
2022-06-13 21:39:46 +0100

    WIP

--- test/hello_test.py
+++ test/hello_test.py
@@ -5,6 +5,7 @@ from src.hello import handler
 
 def test_it_says_hello():
     result = handler({"rawQueryString": "a"}, {"domainName": "b"})
-    assert result["statusCode"] == 200 and result["body"] == json.dumps(
+    assert result["statusCode"] == 200
+    assert result["body"] == json.dumps(
         {"message": "Hello world!", "foo": "a", "bar": "b"}
     )
